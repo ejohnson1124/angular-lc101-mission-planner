d443782cd5b8a2149dd48dbe08c544ad93eb2ea5
Erica <johnson.ericastarr@gmail.com>
2022-07-27 19:51:01 -0500

    commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  missionName: string = "Mars 2030";
-  rocketName: string = "Plasma Max";
+  missionName: string = "LC Angular 3";
+  rocketName: string = "ICAN";
+  editingRocket: boolean = false;
   editingMission: boolean = false;
-
+  
   constructor() { }
 
   ngOnInit() {
@@ -20,8 +21,9 @@ export class HeaderComponent implements OnInit {
     this.missionName = updatedName;
     this.editingMission = false;
   }
+
   updateRocket(updatedName: string) {
-    this.rocketName = updatedName
-    this.editingMission = false;
+    this.rocketName = updatedName;
+    this.editingRocket = false;
   }
 }
